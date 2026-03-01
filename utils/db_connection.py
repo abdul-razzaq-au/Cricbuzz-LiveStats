aa5040006960d490c3f4884f5bb984f466d1c088
Abdul Razzaq <armaaz.au.stats@gmail.com>
2026-03-01 11:20:25 +0530

    Update db_connection.py

--- utils/db_connection.py
+++ utils/db_connection.py
@@ -1,1 +1,16 @@
+# creating SQL connection
+con = psycopg2.connect(
+    host = 'localhost',
+    user = 'postgres',
+    database = 'database_name',
+    password = 'passwprd',
+    port = 5432
+)
+
+# if you dont remember your port:
+# in your database open query tool and type SHOW port;
+# it is mostly 5432 by default
+
+cursor = con.cursor()
+con.autocommit=True
 
